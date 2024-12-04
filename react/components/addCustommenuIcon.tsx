520e091b40d27e9751fad01b759eb42678c5a01d
Fabio Barón <fabio.baron@laburuagencia.com>
2024-12-04 09:18:19 -0500

    avance menu

--- react/components/addCustommenuIcon.tsx
+++ react/components/addCustommenuIcon.tsx
@@ -2,36 +2,36 @@ import React, { useEffect } from 'react'
 
 const AddCustomMenuIcon = () => {
     useEffect(() => {
-        handleaddimage();
-        addCustomClasswithbanner();
+        // handleaddimage();
+        // addCustomClasswithbanner();
     });
-    const handleaddimage = () => {
-        const allSubItems = document.querySelectorAll('.vtex-menu-2-x-submenu .vtex-menu-2-x-menuItem .vtex-menu-2-x-styledLink');
-        if (allSubItems) {
-            allSubItems.forEach((item) => {
-                // const id = item.id;
-                const elem = item.querySelector('.vtex-menu-2-x-styledLinkContent');
-                if (elem) {
-                    if(!elem.querySelector('.vtex-menu-2-x-imageSubmenu')){
-                        const html = `<img class="vtex-menu-2-x-imageSubmenu" src="/arquivos/premier-league.svg?v=1.1">`;
-                        // const html = `<img src="/arquivos/${id}.svg">`;
-                        elem.innerHTML = html + elem.innerHTML;
-                    }                    
-                }
-            })
-        }
-    }
-    const addCustomClasswithbanner = ()=>{
-        const getImage = document.querySelector('.vtex-menu-2-x-submenu .vtex-store-components-3-x-imageElement');
-        if(getImage){
-            const elem = document.querySelector('.vtex-menu-2-x-submenuWrapper--thirdlevel');
-            if(elem){
-                elem.classList.add('vtex-menu-2-x-submenuWrapper--imageSubmenu');
-            }else{
-                document.querySelector('.vtex-menu-2-x-submenuWrapper')?.classList.add('vtex-menu-2-x-submenuWrapper--imageSubmenu');
-            }
-        }
-    }
+    // const handleaddimage = () => {
+    //     const allSubItems = document.querySelectorAll('.vtex-menu-2-x-submenu .vtex-menu-2-x-menuItem .vtex-menu-2-x-styledLink');
+    //     if (allSubItems) {
+    //         allSubItems.forEach((item) => {
+    //             // const id = item.id;
+    //             const elem = item.querySelector('.vtex-menu-2-x-styledLinkContent');
+    //             if (elem) {
+    //                 if(!elem.querySelector('.vtex-menu-2-x-imageSubmenu')){
+    //                     const html = `<img class="vtex-menu-2-x-imageSubmenu" src="/arquivos/premier-league.svg?v=1.1">`;
+    //                     // const html = `<img src="/arquivos/${id}.svg">`;
+    //                     elem.innerHTML = html + elem.innerHTML;
+    //                 }                    
+    //             }
+    //         })
+    //     }
+    // }
+    // const addCustomClasswithbanner = ()=>{
+    //     const getImage = document.querySelector('.vtex-menu-2-x-submenu .vtex-store-components-3-x-imageElement');
+    //     if(getImage){
+    //         const elem = document.querySelector('.vtex-menu-2-x-submenuWrapper--thirdlevel');
+    //         if(elem){
+    //             elem.classList.add('vtex-menu-2-x-submenuWrapper--imageSubmenu');
+    //         }else{
+    //             document.querySelector('.vtex-menu-2-x-submenuWrapper')?.classList.add('vtex-menu-2-x-submenuWrapper--imageSubmenu');
+    //         }
+    //     }
+    // }
     return (<></>)
 }
 
