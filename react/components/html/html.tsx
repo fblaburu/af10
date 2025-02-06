b5dad18257e4ceb419f0476f192682ae30ef4798
j-maker <jhonatan@jumpdigital.co>
2025-02-05 19:17:26 -0500

    Ajustes post entrega

--- react/components/html/html.tsx
+++ react/components/html/html.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const html = () => {
     return (
         <div>
-            <div dangerouslySetInnerHTML={{ __html: '<style>.lf10-mega-menu-3-x-submenuContainer {top: 160px;}</style>' }} />
+            <div dangerouslySetInnerHTML={{ __html: '<style>.lf10-mega-menu-3-x-submenuContainer {top: 155px;}</style>' }} />
         </div>
     );
 };
